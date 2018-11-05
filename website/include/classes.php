195a2f884f650032c17433100a8b7c1390d0bdfd
Corey Robinson <cojaro94@gmail.com>
2018-11-05 12:58:57 -0500

    audit log api

--- website/include/classes.php
+++ website/include/classes.php
@@ -50,9 +50,6 @@ class Application {
         // Declare an errors array
         $errors = [];
 
-        // Connect to the database
-        $dbh = $this->getConnection();
-
         // If a user is logged in, get their userid
         if ($userid == NULL) {
 
@@ -69,19 +66,54 @@ class Application {
             $message = implode( ",", $message);
         }
 
-        // Construct a SQL statement to perform the insert operation
-        $sql = "INSERT INTO auditlog (context, message, logdate, ipaddress, userid) " .
-            "VALUES (:context, :message, NOW(), :ipaddress, :userid)";
+        $url = "https://zcz3dwfpn5.execute-api.us-east-1.amazonaws.com/default/auditlog";
+        $data = array(
+          'context'=>$context,
+          'message'=>$message,
+          'ipaddress'=>$ipaddress,
+          'userid'=>$userid,
+        );
+        $data_json = json_encode($data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('x-api-key: OZ80hhKCvG8ecUWDMTcpGaLAWDswZeMP31Axs9NI', 'Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response  = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($response === FALSE) {
+          $errors[] = "An unexpected failure occurred contacting the web service.";
+        } else {
+          if($httpCode == 400) {
 
-        // Run the SQL select and capture the result code
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(":context", $context);
-        $stmt->bindParam(":message", $message);
-        $stmt->bindParam(":ipaddress", $ipaddress);
-        $stmt->bindParam(":userid", $userid);
-        $stmt->execute();
-        $dbh = NULL;
+            // JSON was double-encoded, so it needs to be double decoded
+            $errorsList = json_decode(json_decode($response))->errors;
+            foreach ($errorsList as $err) {
+              $errors[] = $err;
+            }
+            if (sizeof($errors) == 0) {
+              $errors[] = "Bad input";
+            }
+          } else if($httpCode == 500) {
+            $errorsList = json_decode(json_decode($response))->errors;
+            foreach ($errorsList as $err) {
+              $errors[] = $err;
+            }
+            if (sizeof($errors) == 0) {
+              $errors[] = "Server error";
+            }
+          } else if($httpCode == 200) {
+          }
+        }
 
+        curl_close($ch);
+
+        if (sizeof($errors) == 0){
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 
     protected function validateUsername($username, &$errors) {
@@ -846,15 +878,15 @@ class Application {
       			}
 
       			curl_close($ch);
-              } else {
-                  $this->auditlog("logout error", $errors);
-              }
+          } else {
+              $this->auditlog("logout error", $errors);
+          }
 
-              if (sizeof($errors) == 0){
-                  return TRUE;
-              } else {
-                  return FALSE;
-              }
+          if (sizeof($errors) == 0){
+              return TRUE;
+          } else {
+              return FALSE;
+          }
 
     }
 
