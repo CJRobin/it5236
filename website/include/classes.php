9bd460a122c97667cd0ac62b32e90679c15c4866
Corey Robinson <cojaro94@gmail.com>
2018-11-20 14:58:31 -0500

    updatepassword

--- website/include/classes.php
+++ website/include/classes.php
@@ -618,40 +618,58 @@ class Application {
 
         if(sizeof($errors) == 0) {
 
-            // Connect to the database
-            $dbh = $this->getConnection();
-
             $passwordhash = password_hash($password, PASSWORD_DEFAULT);
 
-            // Construct a SQL statement to perform the select operation
-            $sql = "UPDATE users SET passwordhash=:passwordhash " .
-                "WHERE userid = :userid";
-
-            // Run the SQL select and capture the result code
-            $stmt = $dbh->prepare($sql);
-            $stmt->bindParam(":passwordhash", $passwordhash);
-            $stmt->bindParam(":userid", $userid);
-            $result = $stmt->execute();
-
-            // If the query did not run successfully, add an error message to the list
-            if ($result === FALSE) {
-                $errors[] = "An unexpected error occurred supdating the password.";
-                $this->debug($stmt->errorInfo());
-                $this->auditlog("updateUserPassword error", $stmt->errorInfo());
+            $url = "https://zcz3dwfpn5.execute-api.us-east-1.amazonaws.com/default/updateuserpassword";
+            $data = array(
+              'passwordhash'=>$passwordhash,
+              'userid'=>$userid
+            );
+            $data_json = json_encode($data);
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('x-api-key: OZ80hhKCvG8ecUWDMTcpGaLAWDswZeMP31Axs9NI', 'Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response  = curl_exec($ch);
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            if ($response === FALSE) {
+              $errors[] = "An unexpected error occurred supdating the password.";
+              $this->debug($stmt->errorInfo());
+              $this->auditlog("updateUserPassword error", $stmt->errorInfo());
+              return NULL;
             } else {
+              if($httpCode == 400) {
+
+                // JSON was double-encoded, so it needs to be double decoded
+                $errorsList = json_decode(json_decode($response))->errors;
+                foreach ($errorsList as $err) {
+                  $errors[] = $err;
+                }
+                if (sizeof($errors) == 0) {
+                  $errors[] = "Bad input";
+                }
+              } else if($httpCode == 500) {
+                $errorsList = json_decode(json_decode($response))->errors;
+                foreach ($errorsList as $err) {
+                  $errors[] = $err;
+                }
+                if (sizeof($errors) == 0) {
+                  $errors[] = "Server error";
+                }
+              } else if($httpCode == 200) {
                 $this->auditlog("updateUserPassword", "success");
+              }
             }
 
-            // Close the connection
-            $dbh = NULL;
+            curl_close($ch);
 
         } else {
 
             $this->auditlog("updateUserPassword validation error", $errors);
 
         }
-
-        // Return TRUE if there are no errors, otherwise return FALSE
         if (sizeof($errors) == 0){
             return TRUE;
         } else {
