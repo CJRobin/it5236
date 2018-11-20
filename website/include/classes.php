5f6d9bf7c33d2fcac8da019e8844161165cb64e7
Corey Robinson <cojaro94@gmail.com>
2018-11-20 14:41:11 -0500

    fix

--- website/include/classes.php
+++ website/include/classes.php
@@ -595,7 +595,7 @@ class Application {
               $errors[] = "Server error";
             }
           } else if($httpCode == 200) {
-            $rows = json_decode($response);
+            $rows = json_decode($response, true);
             $regs = array_column($rows, 'registrationcode');
             $this->auditlog("getUserRegistrations", "success");
           }
