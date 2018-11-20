22bbd5663245debce05cddb92c8121e7c4ce5761
Corey Robinson <cojaro94@gmail.com>
2018-11-20 15:11:30 -0500

    clearpasswordrecords

--- website/include/classes.php
+++ website/include/classes.php
@@ -681,18 +681,48 @@ class Application {
     // Does not retrun errors, as the user should not be informed of these problems
     protected function clearPasswordResetRecords($passwordresetid) {
 
-        $dbh = $this->getConnection();
+      $url = "https://zcz3dwfpn5.execute-api.us-east-1.amazonaws.com/default/clearpasswordresetrecords";
+      $data = array(
+        'passwordresetid'=>$passwordresetid
+      );
+      $data_json = json_encode($data);
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('x-api-key: OZ80hhKCvG8ecUWDMTcpGaLAWDswZeMP31Axs9NI', 'Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      $response  = curl_exec($ch);
+      $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      if ($response === FALSE) {
+        $errors[] = "An unexpected error occurred the password.";
+        $this->debug($stmt->errorInfo());
+        return NULL;
+      } else {
+        if($httpCode == 400) {
 
-        // Construct a SQL statement to perform the insert operation
-        $sql = "DELETE FROM passwordreset WHERE passwordresetid = :passwordresetid OR expires < NOW()";
+          // JSON was double-encoded, so it needs to be double decoded
+          $errorsList = json_decode(json_decode($response))->errors;
+          foreach ($errorsList as $err) {
+            $errors[] = $err;
+          }
+          if (sizeof($errors) == 0) {
+            $errors[] = "Bad input";
+          }
+        } else if($httpCode == 500) {
+          $errorsList = json_decode(json_decode($response))->errors;
+          foreach ($errorsList as $err) {
+            $errors[] = $err;
+          }
+          if (sizeof($errors) == 0) {
+            $errors[] = "Server error";
+          }
+        } else if($httpCode == 200) {
+          $this->auditlog("ClearPasswords", "success");
+        }
+      }
 
-        // Run the SQL select and capture the result code
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(":passwordresetid", $passwordresetid);
-        $stmt->execute();
-
-        // Close the connection
-        $dbh = NULL;
+      curl_close($ch);
 
     }
 
